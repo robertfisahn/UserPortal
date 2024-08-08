49b36fcf4d6d44259114be7ecf4bc250e616f7f3
Robert Fisahn <robertfisahn@gmail.com>
2024-08-08 13:28:49 +0200

    Add MongoDb

---
+++ UserPortal/Data/MongoDbSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace UserPortal.Data
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
--- UserPortal/Program.cs
+++ UserPortal/Program.cs
@@ -1,6 +1,23 @@
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using UserPortal.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("MongoDB"));
+builder.Services.AddSingleton<IMongoClient, MongoClient>(s =>
+{
+    var settings = builder.Configuration.GetSection("MongoDB").Get<MongoDbSettings>();
+    return new MongoClient(settings.ConnectionString);
+});
+builder.Services.AddScoped(s =>
+{
+    var client = s.GetRequiredService<IMongoClient>();
+    var settings = s.GetService<IOptions<MongoDbSettings>>().Value;
+    return client.GetDatabase(settings.DatabaseName);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
